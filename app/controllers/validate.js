f02b47f2cc40c075dd37520c58b2b739882afdd9
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-04-19 19:46:54 +0300

    Adds recaptcha validation.

--- app/controllers/validate.js
+++ app/controllers/validate.js
@@ -1,8 +1,38 @@
-const router = require('express').Router();
+const https = require('https'),
+	qs = require('querystring'),
+	router = require('express').Router();
 
 router.post('/', (req, res) => {
-	console.log(res);
-	res.json({ success: true });
+	const postData = qs.stringify({ secret: process.env.RECAPTCHA_SECRET_KEY, response: req.body.response });
+	const options = {
+		host: process.env.RECAPTCHA_HOST,
+		path: process.env.RECAPTCHA_PATH,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': postData.length
+		}
+	};
+
+	var validationRequest = https.request(options, function (validationResponse) {
+		var result = '';
+		validationResponse.on('data', function (chunk) {
+			result += chunk;
+		});
+		validationResponse.on('end', function () {
+			res.send(result);
+		});
+		validationResponse.on('error', function (err) {
+			res.json({ error: err, success: false });
+		});
+	});
+
+	validationRequest.on('error', function (err) {
+		res.json({ error: err, success: false });
+	});
+
+	validationRequest.write(postData);
+	validationRequest.end();
 });
 
 module.exports = (app) => {
